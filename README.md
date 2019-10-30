27331f7c94b8c7cffda11da2c44f2dc332510ec2
Jack Wallace <JWallace@spartaglobal.com>
2019-10-30 16:41:21 +0000

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# python_recipe
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: python_recipe
+# Recipe:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
